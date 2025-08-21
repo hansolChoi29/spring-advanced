adcd21d70c27a452477d2fc6e2d2cb9a26c1964f
hansolChoi29 <ggksthf29@gmail.com>
2025-08-21 17:05:00 +0900

    Lv.4 API로깅 및 Security Config add

--- src/main/java/org/example/expert/domain/comment/controller/CommentAdminController.java
+++ src/main/java/org/example/expert/domain/comment/controller/CommentAdminController.java
@@ -1,10 +1,16 @@
 package org.example.expert.domain.comment.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.comment.service.CommentAdminService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,7 +19,17 @@ public class CommentAdminController {
     private final CommentAdminService commentAdminService;
 
     @DeleteMapping("/admin/comments/{commentId}")
-    public void deleteComment(@PathVariable long commentId) {
+    public void deleteComment(@PathVariable long commentId,
+                              HttpServletRequest request
+    ) {
+        Object roleAttr = request.getAttribute("userRole");
+        String role = roleAttr != null ? roleAttr.toString() : null;
+        if(!"ADMIN".equals(role)){
+            throw   new RuntimeException("권한 없음");
+        }
+
+        System.out.println("[로그]"+ LocalDateTime.now()+"commentId: "+commentId);
+
         commentAdminService.deleteComment(commentId);
     }
 }
--- src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
@@ -1,12 +1,19 @@
 package org.example.expert.domain.user.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.expert.domain.user.service.UserAdminService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +22,16 @@ public class UserAdminController {
     private final UserAdminService userAdminService;
 
     @PatchMapping("/admin/users/{userId}")
-    public void changeUserRole(@PathVariable long userId, @RequestBody UserRoleChangeRequest userRoleChangeRequest) {
+    public void changeUserRole(@PathVariable long userId,
+                               @RequestBody UserRoleChangeRequest userRoleChangeRequest,
+                               HttpServletRequest request
+    ) {
+        Object roleAttr = request.getAttribute("userRole");
+        String role = roleAttr != null ? roleAttr.toString() : null;
+        if(!"ADMIN".equals(role)) {
+            throw new RuntimeException("권한 없음");
+        }
+        System.out.println("[로그]"+ LocalDateTime.now()+"userId: "+userId);
         userAdminService.changeUserRole(userId, userRoleChangeRequest);
     }
 }
